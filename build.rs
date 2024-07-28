9ac8a4d87068e7503e108f8cf82f680395df8656
fxtack <fxtack@qq.com>
2024-07-28 23:20:35 +0800

    feat: add ico

--- build.rs
+++ build.rs
@@ -1,6 +1,12 @@
 use std::process::Command;
 
 fn main () {
+    if cfg!(target_os = "windows") {
+        embed_resource::compile("./projson.rc");
+    } else {
+        panic!("this crate can only be compiled on Windows");
+    }
+
     let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
     let mut build_hash = "0".to_string();
     if output.status.success() {
