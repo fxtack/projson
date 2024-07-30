191da4199beb3dcb79775b01ba828d45832a12ef
fxtack <fxtack@qq.com>
2024-07-30 23:01:25 +0800

    fix: array and struct recursive traversal

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::borrow::Cow;
 use std::fs::File;
 use std::{fs, io};
 use std::io::{Cursor, Read};
-use std::path::{Component, Path, PathBuf};
+use std::path::{Path, PathBuf};
 use clap::{Args, Parser};
 use once_cell::sync::Lazy;
 use serde_json::{Value};
@@ -12,10 +12,16 @@ static VERSION: Lazy<String> = Lazy::new(|| {
     format!("{}.{}", env!("CARGO_PKG_VERSION"), env!("BUILD_HASH"))
 });
 
+fn main() -> anyhow::Result<()> {
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Trace) // 设置日志级别为 Info
+        .init();
+    ProjsonApp::parse().start_and_wait()
+}
+
 #[derive(Args, Debug)]
 #[group(required = true, multiple = false)]
 struct JsonSourceArgs {
-
     /// Specifies the JSON file to read
     #[arg(short, long = "json-file", value_name = "Json file path")]
     file: Option<String>,
@@ -28,7 +34,6 @@ struct JsonSourceArgs {
 #[derive(Debug, Parser)]
 #[command(author, version = VERSION.as_str(), about, arg_required_else_help(true))]
 struct ProjsonApp {
-
     #[command(flatten)]
     json_src: JsonSourceArgs,
 
@@ -68,7 +73,7 @@ impl ProjsonApp {
         log::info!("Virtual path: {}", self.path.display());
 
         log::info!("Press any key to continue...");
-        let _prj = ProjectedFileSystem::new(&self.path, JsonProvider{ val })?;
+        let _prj = ProjectedFileSystem::new(&self.path, JsonProvider { val })?;
         let _ = io::stdin().read(&mut [0u8]).unwrap();
 
         log::info!("Stopped");
@@ -82,26 +87,28 @@ pub struct JsonProvider {
 }
 
 impl JsonProvider {
-    fn get_path<'a>(&self, value: &'a Value, path: &'a Path) -> Option<&'a Value> {
-        let dir_part: Vec<&str> = path.components()
-            .filter_map(|c| match c {
-                Component::Prefix(p) => p.as_os_str().to_str(),
-                Component::RootDir => Some("\\"),
-                Component::CurDir | Component::ParentDir => Some("."),
-                Component::Normal(s) => s.to_str(),
-            })
-            .collect();
-        dir_part.iter().fold(Some(value), |acc, &key| {
-            acc.and_then(|v| match v {
-                Value::Array(arr) => {
-                    key.parse::<usize>()
-                        .ok()
-                        .and_then(|index| arr.get(index))
+    fn get_value_from_path<'a>(value: &'a Value, path: &'a Path) -> Option<&'a Value> {
+        if path.as_os_str().is_empty() {
+            return Some(value);
+        }
+
+        let path_str = path.to_str().unwrap();
+        let parts: Vec<&str> = path_str.split('/').collect();
+
+        let mut current_value = value;
+        for part in parts {
+            match current_value {
+                Value::Object(map) => {
+                    current_value = map.get(part)?;
                 }
-                Value::Object(obj) => obj.get(key),
-                _ => None,
-            })
-        })
+                Value::Array(vec) => {
+                    let index: usize = part.parse().ok()?;
+                    current_value = vec.get(index)?;
+                }
+                _ => return None,
+            }
+        }
+        Some(current_value)
     }
 
     fn value_to_entry(&self, key: String, val: &Value) -> Option<DirectoryEntry> {
@@ -148,11 +155,11 @@ impl JsonProvider {
 impl ProjectedFileSystemSource for JsonProvider {
     fn list_directory(
         &self,
-        path: &Path
+        path: &Path,
     ) -> Vec<DirectoryEntry> {
         log::trace!("access virtual path: '{}'", path.display());
 
-        let curr_path_val = self.get_path(&self.val, path);
+        let curr_path_val = JsonProvider::get_value_from_path(&self.val, path);
         if let None = curr_path_val {
             return Vec::new();
         }
@@ -183,11 +190,11 @@ impl ProjectedFileSystemSource for JsonProvider {
         &self,
         path: &Path,
         byte_offset: usize,
-        length: usize
+        length: usize,
     ) -> io::Result<Box<dyn Read>> {
         log::trace!("access virtual file: '{}'", path.display());
 
-        let curr_path_val = self.get_path(&self.val, path);
+        let curr_path_val = JsonProvider::get_value_from_path(&self.val, path);
         if let None = curr_path_val {
             return Err(io::Error::new(io::ErrorKind::NotFound, "not found"));
         }
@@ -218,9 +225,46 @@ impl ProjectedFileSystemSource for JsonProvider {
     }
 }
 
-fn main() -> anyhow::Result<()> {
-    env_logger::Builder::from_default_env()
-        .filter_level(log::LevelFilter::Trace) // 设置日志级别为 Info
-        .init();
-    ProjsonApp::parse().start_and_wait()
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+    use serde_json::{json};
+    use crate::JsonProvider;
+
+    #[test]
+    fn test_get_value_from_path() {
+        let val = json!({
+            "object": {
+                "key1": "value1",
+                "key2": 2,
+                "key3": true
+            },
+            "array": [1, "two", false, null, { "hello": "world" }, [null, "good", 1024] ],
+            "string": "This is a string.",
+            "number": 123,
+            "boolean": true,
+            "null": null
+        });
+
+        let cases = [
+            (Path::new(""), Some(val.clone())),
+            (Path::new("object"), Some(json!({ "key1": "value1", "key2": 2, "key3": true }))),
+            (Path::new("object/key1"), Some(json!("value1".to_string()))),
+            (Path::new("object/key2"), Some(json!(2))),
+            (Path::new("array/0"), Some(json!(1))),
+            (Path::new("array/1"), Some(json!("two"))),
+            (Path::new("array/4/hello"), Some(json!("world"))),
+            (Path::new("array/5/1"), Some(json!("good"))),
+            (Path::new("boolean"), Some(json!(true))),
+            (Path::new("null"), Some(json!(null))),
+            (Path::new("any"), None)
+        ];
+
+        for case in cases {
+            match JsonProvider::get_value_from_path(&val, &case.0) {
+                None => assert_eq!(case.1, None),
+                Some(expected) => assert_eq!(*expected, case.1.unwrap()),
+            }
+        }
+    }
 }
